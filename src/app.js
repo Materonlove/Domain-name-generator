7b0ef48b51222eb1f121d3a4e18ea187e90ae8f8
Materonlove <materonlove@gmail.com>
2023-02-14 20:58:36 +0000

    tarea terminada

--- src/app.js
+++ src/app.js
@@ -8,4 +8,28 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  document.querySelector("button").addEventListener("click", ejecutar);
+};
+
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+
+const ejecutar = () => {
+  let texto;
+  let parrafo;
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      console.log(pronoun[i] + adj[j]);
+      for (let k = 0; k < noun.length; k++) {
+        console.log(pronoun[i] + adj[j] + noun[k]);
+        parrafo = document.createElement("p");
+        texto = document.createTextNode(pronoun[i] + adj[j] + noun[k] + ".com");
+        parrafo.appendChild(texto);
+        document.querySelector("#resultado-Dominios").appendChild(parrafo);
+      }
+    }
+  }
 };
